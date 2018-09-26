f2e56218d85e5be313554abd2dbb8bbbd17bed0e
Hes Siemelink <hsiemelink@xebialabs.com>
2018-09-26 09:55:59 +0200

    Moves sample release out of the way of importing dashboards

--- demo/JenkinsWorld/insert.sh
+++ demo/JenkinsWorld/insert.sh
@@ -1,4 +1,4 @@
-find releases -type f -name "*.yaml" -exec python -m yay releases/insert_release.yay folder="Cool Store" file={} \;
+find "releases/Cool Store" -type f -name "*.yaml" -exec python -m yay releases/insert_release.yay folder="Cool Store" file={} \;
 python -m yay dashboards/add_dashboards.yay folder="Cool Store" file=dashboards/cool_store_dashboard.yaml
 python -m yay dashboards/add_dashboards.yay folder="Cool Store/Address book" file=dashboards/address_book_dashboard.yaml
 python -m yay dashboards/add_dashboards.yay folder="Cool Store/Shopping cart" file=dashboards/shopping_cart_dashboard.yaml
